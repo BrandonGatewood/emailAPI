169805382d581d91634eba3f1dfe33cff9696b85
Brandon Gatewood <90942107+BrandonGatewood@users.noreply.github.com>
2025-08-16 13:53:04 -0700

    added middleware to app

--- Program.cs
+++ Program.cs
@@ -1,15 +1,16 @@
 using DotNetEnv;
 using emailAPI.Services;
+using emailAPI.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 
 Env.Load();
-
 builder.Services.AddSingleton<EmailService>();
 
 builder.Services.AddControllers();
 
 var app = builder.Build();
 
+app.UseMiddleware<ApiKeyMiddleware>();
 app.MapControllers();
 app.Run();
