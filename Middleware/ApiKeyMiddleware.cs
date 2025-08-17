bfd482c0eec693191201c4ca2c9c972aac7a0e1c
Brandon Gatewood <90942107+BrandonGatewood@users.noreply.github.com>
2025-08-17 10:50:11 -0700

    updated ApiKeyMiddleware implemented Iconfiguration instead of GetEnvironmentVariable cached api key bytes use fixedTimeEquals for constant time comparison return generic 401 so no sensitive information is returned

--- Middleware/ApiKeyMiddleware.cs
+++ Middleware/ApiKeyMiddleware.cs
@@ -1,17 +1,22 @@
+using System.Security.Cryptography;
+using System.Text;
+
 namespace emailAPI.Middleware
 {
-    public class ApiKeyMiddleware(RequestDelegate next)
+    public class ApiKeyMiddleware(RequestDelegate next, IConfiguration config)
     {
         private readonly RequestDelegate _next = next;
-        private readonly string _apiKey = Environment.GetEnvironmentVariable("API_KEY")
-            ?? throw new InvalidOperationException("API_KEY is not set in environment variables.");
-
+        private readonly byte[] _apiKeyBytes = Encoding.UTF8.GetBytes(
+            config["API_KEY"] ?? throw new InvalidOperationException("API key is not set in environment variables.")
+        );
         public async Task InvokeAsync(HttpContext context)
         {
-            if (!context.Request.Headers.TryGetValue("x-api-key", out var key) || key != _apiKey)
+            // check if header has api key or compare every character in constant time
+            if (!context.Request.Headers.TryGetValue("x-api-key", out var key) ||
+                !CryptographicOperations.FixedTimeEquals(
+                    Encoding.UTF8.GetBytes(key.ToString()), _apiKeyBytes))
             {
-                context.Response.StatusCode = 401;
-                await context.Response.WriteAsync("Unauthorized");
+                context.Response.StatusCode = StatusCodes.Status401Unauthorized;
                 return;
             }
 
