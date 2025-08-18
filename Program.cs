bc9263f87051cc1e1dc8f2e02e3a573dd0eac9f0
Brandon Gatewood <90942107+BrandonGatewood@users.noreply.github.com>
2025-08-18 13:39:01 -0700

    added ratelimitermiddleware to api

--- Program.cs
+++ Program.cs
@@ -1,11 +1,13 @@
 using DotNetEnv;
 using emailAPI.Services;
 using emailAPI.Middleware;
+using emailAPI.Extensions;
 
 Env.Load();
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddRedis(builder.Configuration);
 builder.Services.AddSingleton<IEmailService, EmailService>();
 
 builder.Services.AddControllers();
@@ -13,5 +15,6 @@ builder.Services.AddControllers();
 var app = builder.Build();
 
 app.UseMiddleware<ApiKeyMiddleware>();
+app.UseMiddleware<RateLimiterMiddleware>();
 app.MapControllers();
 app.Run();
